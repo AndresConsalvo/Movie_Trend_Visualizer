50d39a287815f8165af0b2000abf4d1b0bd1639d
lach9859 <73550815+lach9859@users.noreply.github.com>
2022-04-18 23:25:50 -0400

    Added popup window to check number of tuples

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Popup from './Popup';
+import Axios from 'axios';
 
 function About() {
+  const [tupleCount, setTupleCount] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/tuplecount').then((response) => {
+      setTupleCount(response.data);
+    });
+  }, []);
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <div className="about">
       <div class="container">
@@ -22,6 +37,26 @@ function About() {
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
+          </div>
+          <div class="col-lg-6">
+            <input
+              type="button"
+              value="Count tuples"
+              onClick={togglePopup}
+            />
+            {isOpen && <Popup
+              content={<>
+                <h4 class="font-weight-normal">Movie tuples: {tupleCount[0].NUMROWS}</h4>
+                <h4 class="font-weight-normal">Member tuples: {tupleCount[1].NUMROWS}</h4>
+                <h4 class="font-weight-normal">Cast member tuples: {tupleCount[2].NUMROWS}</h4>
+                <h4 class="font-weight-normal">Production company tuples: {tupleCount[3].NUMROWS}</h4>
+                <h4 class="font-weight-normal">Genre tuples: {tupleCount[4].NUMROWS}</h4>
+                <h4 class="font-weight-normal">Movie rating tuples: {tupleCount[5].NUMROWS}</h4>
+                <br></br>
+                <h4>Total tuples: {tupleCount[6].NUMROWS}</h4>
+              </>}
+              handleClose={togglePopup}
+            />}
           </div>
         </div>
       </div>
